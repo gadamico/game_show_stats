05ec0373e878c9d287ca933401e5c3bd5a2bdbe9
Greg Damico <greg.damico@flatironschool.com>
2022-02-26 17:26:50 -0800

    re-structure into multiple ntbks

--- src/functions.py
+++ src/functions.py
@@ -34,6 +34,73 @@ def prob_plyr3_ties_then_loses(plyr2):
     
     return two_spins + one_spin
 
+def plyr3_too_small_given_first_spin_plyr2(spin1):
+    """This function returns the probability that Player 3's
+    total will be under Player 2's total given the value of
+    Player 2's first spin as input."""
+    
+    import numpy as np
+    out = 0
+    for spin2 in np.arange(1, 21-spin1):
+        for z in np.arange(1, spin1+spin2-1):
+            out += spin1 + spin2 - 1 - z
+    return out / 8000
+
+def plyr3_goes_over_given_first_spin_plyr2(spin1):
+    """This function returns the probability that Player 3's
+    total will be over $1.00 (after failing to match or exceed
+    Player 2's total on the first spin), given the value of
+    Player 2's first spin as input."""
+    
+    import numpy as np
+    out = 0
+    for spin2 in np.arange(1, 21-spin1):
+        for z in np.arange(1, spin1+spin2):
+            out += z
+    return out / 8000
+
+def plyr3_ties_then_loses_given_first_spin_plyr2(spin1):
+    """This function returns the probability that Player 3
+    ties Player 2 on one or two spins and then loses, either
+    by exceeding $1.00 or by losing in a one-spin playoff,
+    given the value of Player 2's first spin as input.
+    We assume here the optimal strategy for Player 3 when
+    tying Player 2 on the first spin of spinning again when
+    the score is 50 cents or less and otherwise staying (and
+    waiting for the one-spin playoff)."""
+    
+    import numpy as np
+    out = 0
+    start = max(11-spin1, 1)
+    for spin2 in np.arange(1, 11-spin1):
+        out += (spin1+spin2) / 20 + (spin1+spin2-1) / 40
+    for spin2 in np.arange(start, 21-spin1):
+        out += 1 / 2 + (spin1+spin2-1) / 40
+    return out / 400
+
+def plyr2_defeats_plyr3(plyr2, spin_again=True):
+    """This function returns the probability that Player 2
+    defeats Player 3, given the value of Player 2's first
+    spin as input. This function assumes that Player 1 is
+    out of the picture (either because of exceeding $1.00
+    or by having a total less than Player 2's first spin."""
+    
+    import numpy as np
+    prob = 1
+    if spin_again:
+        plyr3_too_small = plyr3_too_small_given_first_spin_plyr2(plyr2)
+        plyr3_goes_over = plyr3_goes_over_given_first_spin_plyr2(plyr2)
+        plyr3_ties_then_loses = plyr3_ties_then_loses_given_first_spin_plyr2(plyr2)
+    
+    else:
+        plyr3_too_small = prob_plyr3_too_small(plyr2)
+        plyr3_goes_over = prob_plyr3_goes_over(plyr2)
+        plyr3_ties_then_loses = prob_plyr3_ties_then_loses(plyr2)
+    
+    prob *= (plyr3_too_small + plyr3_goes_over + plyr3_ties_then_loses)
+    
+    return prob
+
 def first_player():
     """This function simulates the turn of the first person
     in the Showcase Showdown."""
