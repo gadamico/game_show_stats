984efbc4a0fa566ff43016de227b25b7c7ca1943
Greg Damico <greg.damico@flatironschool.com>
2022-03-30 15:15:48 -0600

    add docstrings and complete ntbks

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,5 @@
+## Functions for Analysis
+
 def prob_plyr3_too_small(plyr2):
     """This function returns the probability that Player 3's
     total will be under Player 2's (input)."""
@@ -732,6 +734,94 @@ def plyr2_exceeds_plyr3_ties_then_loses_given_first_spin_plyr1(spin1):
         prob += plyr2*plyr3
     return prob / 20**4
 
+def plyr1_wins(spin1, spin_again=True):
+    """This function returns the probability that Player 1
+    wins, given the value of Player 1's first
+    spin as input. This function assumes that Players 2 and 3
+    employ optimal strategy."""
+    
+    import numpy as np
+    win_prob = 1
+    if spin_again:
+        plyr2_plyr3_too_small = plyr2_and_plyr3_too_small_given_first_spin_plyr1(spin1)
+        plyr2_too_small_plyr3_goes_over =\
+            plyr2_too_small_and_plyr3_goes_over_given_first_spin_plyr1(spin1)
+        plyr2_goes_over_plyr3_too_small =\
+            plyr2_goes_over_and_plyr3_too_small_given_first_spin_plyr1(spin1)
+        plyr2_plyr3_go_over = plyr2_and_plyr3_go_over_given_first_spin_plyr1(spin1)
+        plyr2_ties_exceeds_plyr3_too_small =\
+            plyr2_ties_then_exceeds_plyr3_too_small_given_first_spin_plyr1(spin1)
+        plyr2_ties_exceeds_plyr3_exceeds =\
+            plyr2_ties_then_exceeds_plyr3_exceeds_given_first_spin_plyr1(spin1)
+        plyr2_ties_exceeds_plyr3_ties_loses =\
+            plyr2_ties_then_exceeds_plyr3_ties_then_loses_given_first_spin_plyr1(spin1)
+        plyr2_ties_loses_plyr3_too_small =\
+            plyr2_ties_then_loses_plyr3_too_small_given_first_spin_plyr1(spin1)
+        plyr2_ties_loses_plyr3_exceeds =\
+            plyr2_ties_then_loses_plyr3_exceeds_given_first_spin_plyr1(spin1)
+        plyr2_ties_loses_plyr3_ties_loses =\
+            plyr2_ties_then_loses_plyr3_ties_then_loses_given_first_spin_plyr1(spin1)
+        plyr2_ties_on2_loses_plyr3_too_small =\
+            plyr2_ties_on2_then_loses_plyr3_too_small_given_first_spin_plyr1(spin1)
+        plyr2_ties_on2_loses_plyr3_exceeds =\
+            plyr2_ties_on2_then_loses_plyr3_exceeds_given_first_spin_plyr1(spin1)
+        plyr2_ties_on2_loses_plyr3_ties_loses =\
+            plyr2_ties_on2_then_loses_plyr3_ties_then_loses_given_first_spin_plyr1(spin1)
+        plyr2_too_small_plyr3_ties_exceeds =\
+            plyr2_too_small_plyr3_ties_then_exceeds_given_first_spin_plyr1(spin1)
+        plyr2_too_small_plyr3_ties_loses =\
+            plyr2_too_small_plyr3_ties_then_loses_given_first_spin_plyr1(spin1)
+        plyr2_exceeds_plyr3_ties_exceeds =\
+            plyr2_exceeds_plyr3_ties_then_exceeds_given_first_spin_plyr1(spin1)
+        plyr2_exceeds_plyr3_ties_loses =\
+            plyr2_exceeds_plyr3_ties_then_loses_given_first_spin_plyr1(spin1)
+    
+    else:
+        plyr2_plyr3_too_small = prob_plyr2_and_plyr3_too_small(spin1)
+        plyr2_too_small_plyr3_goes_over = plyr2_too_small_and_plyr3_goes_over(spin1)
+        plyr2_goes_over_plyr3_too_small = plyr2_goes_over_and_plyr3_too_small(spin1)
+        plyr2_plyr3_go_over = plyr2_and_plyr3_go_over(spin1)
+        plyr2_ties_exceeds_plyr3_too_small =\
+            plyr2_ties_then_exceeds_plyr3_too_small(spin1)
+        plyr2_ties_exceeds_plyr3_exceeds = plyr2_ties_then_exceeds_plyr3_exceeds(spin1)
+        plyr2_ties_exceeds_plyr3_ties_loses =\
+            plyr2_ties_then_exceeds_plyr3_ties_then_loses(spin1)
+        plyr2_ties_loses_plyr3_too_small = plyr2_ties_then_loses_plyr3_too_small(spin1)
+        plyr2_ties_loses_plyr3_exceeds = plyr2_ties_then_loses_plyr3_exceeds(spin1)
+        plyr2_ties_loses_plyr3_ties_loses =\
+            plyr2_ties_then_loses_plyr3_ties_then_loses(spin1)
+        plyr2_ties_on2_loses_plyr3_too_small =\
+            plyr2_ties_on2_then_loses_plyr3_too_small(spin1)
+        plyr2_ties_on2_loses_plyr3_exceeds =\
+            plyr2_ties_on2_then_loses_plyr3_exceeds(spin1)
+        plyr2_ties_on2_loses_plyr3_ties_loses =\
+            plyr2_ties_on2_then_loses_plyr3_ties_then_loses(spin1)
+        plyr2_too_small_plyr3_ties_exceeds =\
+            plyr2_too_small_plyr3_ties_then_exceeds(spin1)
+        plyr2_too_small_plyr3_ties_loses = plyr2_too_small_plyr3_ties_then_loses(spin1)
+        plyr2_exceeds_plyr3_ties_exceeds = plyr2_exceeds_plyr3_ties_then_exceeds(spin1)
+        plyr2_exceeds_plyr3_ties_loses = plyr2_exceeds_plyr3_ties_then_loses(spin1)
+    
+    win_prob *= (plyr2_plyr3_too_small + plyr2_too_small_plyr3_goes_over\
+                 + plyr2_goes_over_plyr3_too_small + plyr2_plyr3_go_over\
+                 + plyr2_ties_exceeds_plyr3_too_small\
+                 + plyr2_ties_exceeds_plyr3_exceeds\
+                 + plyr2_ties_exceeds_plyr3_ties_loses\
+                 + plyr2_ties_loses_plyr3_too_small\
+                 + plyr2_ties_loses_plyr3_exceeds\
+                 + plyr2_ties_loses_plyr3_ties_loses\
+                 + plyr2_ties_on2_loses_plyr3_too_small\
+                 + plyr2_ties_on2_loses_plyr3_exceeds\
+                 + plyr2_ties_on2_loses_plyr3_ties_loses\
+                 + plyr2_too_small_plyr3_ties_exceeds\
+                 + plyr2_too_small_plyr3_ties_loses\
+                 + plyr2_exceeds_plyr3_ties_exceeds\
+                 + plyr2_exceeds_plyr3_ties_loses)
+    
+    return win_prob
+
+## Functions for Simulation
+
 def first_player():
     """This function simulates the turn of the first person
     in the Showcase Showdown."""
@@ -807,3 +897,294 @@ def second_player(play1):
             play2 = spin1 + spin2
             print(f'Player2 has {play2}.')
     return play2
+
+def third_player(play1, play2):
+    """This function simulates the turn of the third person
+    in the Showcase Showdown."""
+    
+    import numpy as np
+    scores = dict(enumerate([play1, play2], start=1))
+    leader = {k: v for k, v in scores.items() if v == max(scores.values())}
+    leader_val = [v for v in leader.values()][0]
+    if len(leader) == 2:
+        print(f'Player1 and Player2 are tied at {play1}!')
+    else:
+        leader_num = [k for k in leader.keys()][0]
+        print(f'Player{leader_num} has {leader_val}.')
+    choices = [5*i for i in range(1, 21)]
+    spin1 = np.random.choice(choices)
+    print(f'Player3 spun {spin1}.')
+    
+    ## Case1. Player3 defeats the leader(s) on one spin.
+    ## Note: Even in this case, Player3
+    ## may spin again to go for the $1.00.
+    if spin1 > leader_val:
+        print(f'Does Player3 want to go for the $1.00?')
+        choice = input('Yes or No: ')
+        if choice == 'Yes':
+            spin2 = np.random.choice(choices)
+            if spin1 + spin2 > 100:
+                play3 = 0
+                print('Player3 is over!')
+            else:
+                play3 = spin1 + spin2
+                print(f'Player3 wins with {play3}.')
+        else:
+            play3 = spin1
+            print(f'Player3 wins with {play3}.')
+        return play3
+    
+    ## Case 2. Player3 ties the leader(s).
+    elif spin1 == leader_val:
+        if len(leader) == 2:
+            print(f'We have a three-way tie at {leader_val}!')
+            print(f'Does Player3 want to spin again?')
+            choice = input('Yes or No: ')
+            if choice == 'Yes':
+                spin2 = np.random.choice(choices)
+                if spin1 + spin2 > 100:
+                    play3 = 0
+                    print('Player3 is over!')
+                else:
+                    play3 = spin1 + spin2
+                    print(f'Player3 wins with {play3}.')
+            else:
+                print(f'Each player gets one more spin.')
+                play1f = np.random.choice(choices)
+                print(f'Player1 spun {play1f}.')
+                play2f = np.random.choice(choices)
+                print(f'Player2 spun {play2f}.')
+                if play2f == play1f:
+                    print('Player2 must spin again.')
+                    play2f += np.random.choice(choices)
+                    if play2f > 100:
+                        print('Player2 is over!')
+                        play2f = 0
+                elif play2f < play1f:
+                    play2f = 0
+                else:
+                    play1f = 0
+                    print(f'Player2 leads with {play2f}.')
+                play3f = np.random.choice(choices)
+                print(f'Player3 spun {play3f}.')
+                if play3f == play1f or play3f == play2f:
+                    print('Player3 must spin again.')
+                    play3f += np.random.choice(choices)
+                    if play3f > 100:
+                        print('Player3 is over!')
+                        play3f = 0
+                finals = dict(enumerate([play1f, play2f, play3f], start=1))
+                winner = {k: v for k, v in finals.items() if v == max(finals.values())}
+                print(f'The winner is {[k for k in winner.keys()][0]}!')
+                return play3f
+        else:
+            print(f'We have a tie at {leader_val}!')
+            print(f'Does Player3 want to spin again?')
+            choice = input('Yes or No: ')
+            if choice == 'Yes':
+                spin2 = np.random.choice(choices)
+                if spin1 + spin2 > 100:
+                    play3 = 0
+                    print('Player3 is over!')
+                else:
+                    play3 = spin1 + spin2
+                    print(f'Player3 wins with {play3}.')
+            else:
+                print(f'We have a tie between Player3 and Player{leader_num}.')
+                print(f'Each player gets one more spin.')
+                play1f = np.random.choice(choices)
+                print(f'Player{leader_num} spun {play1f}.')
+                play2f = np.random.choice(choices)
+                print(f'Player3 spun {play2f}.')
+                if play2f == play1f:
+                    print('Player3 must spin again.')
+                    play2f += np.random.choice(choices)
+                    if play2f > 100:
+                        print('Player3 is over!')
+                        play2f = 0
+                elif play2f < play1f:
+                    play2f = 0
+                    print(f'Player3 can\'t beat Player{leader_num}!')
+                else:
+                    play1f = 0
+                    print(f'Player3 wins with {play2f}.')
+    
+    ## Case 3. Player3 is under the leader(s) and must spin again.
+    else:
+        print('Player3 must spin again.')
+        spin2 = np.random.choice(choices)
+        print(f'Player3 spun {spin2}.')
+        if spin1 + spin2 > 100:
+            play3 = 0
+            print('Player3 is over!')
+        elif spin1 + spin2 < leader_val:
+            play3 = 0
+            print('Player3 can\'t beat Player1 and Player2!')
+        else:
+            play3 = spin1 + spin2
+            if play3 > leader_val:
+                print(f'Player3 wins with {play3}.')
+            else:
+                print(f'We have a tie between Player3 and Player{leader_num}.')
+                print(f'Each player gets one more spin.')
+                play1f = np.random.choice(choices)
+                print(f'Player{leader_num} spun {play1f}.')
+                play2f = np.random.choice(choices)
+                print(f'Player3 spun {play2f}.')
+                if play2f == play1f:
+                    print('Player3 must spin again.')
+                    play2f += np.random.choice(choices)
+                    if play2f > 100:
+                        print('Player3 is over!')
+                        play2f = 0
+                elif play2f < play1f:
+                    play2f = 0
+                    print(f'Player3 can\'t beat Player{leader_num}!')
+                else:
+                    play1f = 0
+                    print(f'Player3 wins with {play2f}.')
+        return play3
+    
+def play_full():
+    """This function simulates a complete Showcase Showdown."""
+    
+    play1 = first_player()
+    play2 = second_player(play1)
+    play3 = third_player(play1, play2)
+
+## Functions (and 'wheel' definition) for Statistics    
+
+wheel = [i for i in range(5, 101, 5)]
+
+def stats_player1(choices=wheel, spin_again=True, spin_first=None,
+                 opt_strategy=False):
+    """This function returns a score for Player1 with optional
+    settings for a first spin, whether or not to follow the
+    optimal strategy, and whether or not to spin again (the
+    optimal strategy notwithstanding)."""
+    
+    import numpy as np
+    if spin_first is None:
+        spin1 = np.random.choice(choices)
+    else:
+        spin1 = spin_first
+    if opt_strategy:
+        if spin1 > 65:
+            spin_again=False
+        else:
+            spin_again=True
+    if spin_again:
+        spin2 = np.random.choice(choices)
+        if spin1 + spin2 > 100:
+            play1 = 0
+        else:
+            play1 = spin1 + spin2
+    else:
+        play1 = spin1
+    return play1
+
+def stats_player2(play1, choices=wheel, spin_again=True, spin_first=None,
+                 opt_strategy=False):
+    """This function returns a score for Player2 given a score
+    for Player1, with optional settings for a first spin,
+    whether or not to follow the optimal strategy, and whether
+    or not to spin again (the optimal strategy notwithstanding)."""
+    
+    import numpy as np
+    if spin_first is None:
+        spin1 = np.random.choice(choices)
+    else:
+        spin1 = spin_first
+    if opt_strategy:
+        if spin1 == play1:
+            if play1 > 65:
+                spin_again = False
+        elif spin1 > 50:
+            spin_again = False
+        else:
+            spin_again = True
+    if spin1 < play1:
+        spin_again = True
+    if spin_again:
+        spin2 = np.random.choice(choices)
+        if spin1 + spin2 > 100 or spin1 + spin2 < play1:
+            play2 = 0
+        else:
+            play2 = spin1 + spin2
+    else:
+        play2 = spin1
+    return play2
+
+def stats_player3(leader, tie=False, choices=wheel, spin_again=True,
+                  spin_first=None, opt_strategy=False, gofordollar=False):
+    """This function returns a score for Player3 given a leading
+    score, with optional settings for a first spin, whether
+    Players 1 and 2 are tied , whether or not to follow the
+    optimal strategy, whether or not to spin again (the
+    optimal strategy notwithstanding), and whether or not to
+    go for a dollar (after defeating the leader(s) one one spin)."""
+    
+    import numpy as np
+    if spin_first is None:
+        spin1 = np.random.choice(choices)
+    else:
+        spin1 = spin_first
+    if opt_strategy:
+        if spin1 == leader:
+            if tie:
+                if leader > 65:
+                    spin_again = False
+                else:
+                    spin_again = True
+            else:
+                if leader > 50:
+                    spin_again = False
+                else:
+                    spin_again = True
+    if spin1 < leader:
+        spin_again = True
+    if leader == 0:
+        spin_again = False
+    if spin1 > leader:
+        if gofordollar == True:
+            spin_again = True
+        else:
+            spin_again = False
+    if spin_again:
+        spin2 = np.random.choice(choices)
+        if spin1 + spin2 > 100 or spin1 + spin2 < leader:
+            play3 = 0
+        else:
+            play3 = spin1 + spin2
+    else:
+        play3 = spin1
+    return play3
+
+def showcase_showdown(plr1_first, plr1_again, plr2_first, plr2_again,
+                      plr3_first, plr3_again, plr1os=False, plr2os=False,
+                      plr3os=False, plr3gfd=False, tie=False):
+    """This function simulates a complete Showcase Showdown with all
+    of the options from the individual player functions above."""
+    
+    import numpy as np
+    wheel = [i for i in range(5, 101, 5)]
+    plr1 = stats_player1(spin_again=plr1_again, spin_first=plr1_first,
+                        opt_strategy=plr1os)
+    plr2 = stats_player2(play1=plr1, spin_again=plr2_again,
+                         spin_first=plr2_first, opt_strategy=plr2os)
+    if plr1 > plr2:
+        leader = plr1
+    else:
+        leader = plr2
+    plr3 = stats_player3(leader, spin_again=plr3_again,
+                         spin_first=plr3_first, opt_strategy=plr3os,
+                         gofordollar=plr3gfd, tie=tie)
+    
+    score_dict = dict(enumerate([plr1, plr2, plr3], start=1))
+    
+    wnr = {k: v for k, v in score_dict.items() if v == max(score_dict.values())}
+    
+    if len(wnr) > 1:
+        return np.random.choice([k for k in wnr.keys()])
+    else:
+        return [k for k in wnr.keys()][0]
